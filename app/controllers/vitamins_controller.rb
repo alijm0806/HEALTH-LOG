2c0e76adcf87986fbe86d99ebee27e3c7110ce2c
alijm0806 <alijm1104@gmail.com>
2022-08-26 15:39:48 -0500

    Add user authentification

--- app/controllers/vitamins_controller.rb
+++ app/controllers/vitamins_controller.rb
@@ -1,2 +1,14 @@
 class VitaminsController < ApplicationController
+  def index
+    vitamins = Vitamin.all
+    render json: vitamins.as_json
+  end
+
+  def show
+    if current_user
+      vitamin = Vitamin.find_by(id: params[:id])
+
+      render json: vitamin.as_json
+    end
+  end
 end
