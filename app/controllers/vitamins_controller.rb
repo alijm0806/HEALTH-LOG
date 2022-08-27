3f594b31b17a663a4ac58543b565d125b6054ec8
alijm0806 <alijm1104@gmail.com>
2022-08-27 07:32:12 -0500

    Add jb gem / add index.json and show.json

--- app/controllers/vitamins_controller.rb
+++ app/controllers/vitamins_controller.rb
@@ -1,34 +1,36 @@
 class VitaminsController < ApplicationController
-  before_action :authenticate_admin, only: [:create, :update, :destroy]
+  # before_action :authenticate_admin, only: [:create, :update, :destroy]
 
   def index
-    vitamins = Vitamin.all
-    render json: vitamins.as_json
+    @vitamins = Vitamin.all
+    render template: "vitamins/index"
   end
 
   def show
-    vitamin = Vitamin.find_by(id: params[:id])
-    render json: vitamin.as_json
+    @vitamin = Vitamin.find_by(id: params[:id])
+    render template: "vitamins/show"
   end
 
   def create
-    vitamin = Vitamin.new(
+    @vitamin = Vitamin.new(
       name: params[:name],
+      description: params[:description],
     )
-    vitamin.save
-    render json: vitamin.as_json
+    @vitamin.save
+    render template: "vitamins/show"
   end
 
   def update
-    vitamin = Vitamin.find_by(id: params[:id])
-    vitamin.name = params[:name]
-    vitamin.save
-    render json: vitamin.as_json
+    @vitamin = Vitamin.find_by(id: params[:id])
+    @vitamin.name = params[:name]
+    @vitamin.description = params[:description]
+    @vitamin.save
+    render template: "vitamins/show"
   end
 
   def destroy
-    vitamin = Vitamin.find_by(id: params[:id])
-    vitamin.destroy
-    render json: { message: "The selected vitamins has been removed" }
+    @vitamin = Vitamin.find_by(id: params[:id])
+    @vitamin.destroy
+    render json: { message: "you have removed the selected product" }
   end
 end
