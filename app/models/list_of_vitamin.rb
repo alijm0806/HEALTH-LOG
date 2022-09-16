3b21d6c23b7416b53d63d4cbe472d7e984e3d601
alijm0806 <alijm1104@gmail.com>
2022-09-15 21:06:43 -0500

    Add stats to listofvitamins + add validation for quantity numericality

--- app/models/list_of_vitamin.rb
+++ app/models/list_of_vitamin.rb
@@ -2,4 +2,6 @@ class ListOfVitamin < ApplicationRecord
   belongs_to :vitamin
   belongs_to :user
   validates_uniqueness_of :vitamin_id, uniqueness: true, scope: :user_id
+  validates :quantity, numericality: true, format: { with: /\A[+-]?\d+\z/,
+                                                     message: "only allows numbers" }
 end
