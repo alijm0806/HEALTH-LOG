a851ebaf3180374b182b38468af9a312a44dba35
alijm0806 <alijm1104@gmail.com>
2022-09-30 15:23:10 -0500

    Update cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "localhost:8080"
+    origins "localhost:8080", "https://visionary-brioche-5cb49f.netlify.app"
 
     resource "*",
       headers: :any,
